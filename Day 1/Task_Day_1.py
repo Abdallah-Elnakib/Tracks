414b99a2339e87af0f566275793a5190a3b3bb98
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-05-18 10:52:44 +0300

    python track day 1

---
+++ Day 1/Task_Day_1.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------------------------------------------
+# Write a Python program that calculates the area of a circle based on the radius entered by the user.
+# ------------------------------------------------------------------------------------------------------------------
+# Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+# ------------------------------------------------------------------------------------------------------------------
+# Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+# Sample value of n is 5
+# Expected Result : 615
+# ------------------------------------------------------------------------------------------------------------------
