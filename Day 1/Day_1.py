08c6fc8e0c48e3fae53ac106793b3e23bec197a5
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-05-18 12:30:54 +0300

    print

---
+++ Day 1/Day_1.py
@@ -0,0 +1,31 @@
+# var 
+# +
+# -
+# *
+# /
+# 'fghjkl;'
+# "tyiuopo;m"
+
+# int , str
+
+# print(5 + 'abdallah')   xxxxxxxxxxxxxxxx
+
+
+# print('Abdallah' + 'Elnakib')
+# print('Abdallah' - 'Elnakib')     xxxxxxxxxxxxx
+# print('Abdallah' * 'Elnakib')     xxxxxxxxxxxxx
+# print('Abdallah' / 'Elnakib')     xxxxxxxxxxxxx
+# print(5+5)
+
+
+
+# fffffffffffffffffffffff
+
+# هذا الكود يستخد علشان اعمل 
+
+# x = 5    # متغير يساوي رقم 5
+
+# if x  == 5 :
+#     print('hello')
+
+
