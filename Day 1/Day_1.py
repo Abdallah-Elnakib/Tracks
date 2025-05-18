7d25e9cb92a9e740a561570457139db1ead10ba9
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-05-18 17:01:17 +0300

    add more Data

--- Day 1/Day_1.py
+++ Day 1/Day_1.py
@@ -29,3 +29,192 @@
 #     print('hello')
 
 
+
+# print('abdallah' , 'elnakik')
+# print('abdallah' +  ' ' +'elnakik')
+# print('abdallah' + ' elnakik')
+
+
+# print(5 + 5)
+# print("5" + "5")
+
+
+# 10055945524 => 'abdallah elnakib'
+
+# var  => str int  dic list floot tuble range bool
+
+# abdallah = 159874235
+# name = 'abdallah ayman'
+
+# print(name)
+# print(name)
+# print(name)
+# print(name)
+# print(name)
+# print(abdallah, name)
+
+
+# print(5)
+
+# if True :
+#     print(5555555555555555555)
+
+# ahmed  = 55555
+
+# print(ahmed)
+# print("ahmed")
+
+# 5name = 555
+#  name = 555
+# n+5 = 555
+# abdallah Elnakib  =555
+
+# x = 5
+# y = 9
+# mohamed = 5555
+
+
+# print  = 5555
+
+# print(print)
+
+# x = 5 
+# x = 10
+# x = 15
+# print(x)
+
+# 5nmae = 5
+
+# Print = 55
+
+# print(5)
+
+
+# x = 5 + 5 + 10 -1
+
+# print(x)
+
+# Print = 5 
+# print(Print)
+
+# print(5 + 5)
+
+
+# x = 'abdallah'
+# y = 'elnakib'
+
+# print(x , y)
+
+
+
+# name = input('Username : ')
+
+# print(name + 5)
+
+# print()
+# input()
+# int() 
+# str()
+
+# x = 10
+# y = 15
+
+# print(x + y)
+# print(int(x) + int(y))
+
+# print(str(x) + str(y))
+
+# name = input('Your Name : ')
+
+# number1 = int(input('Num1 : '))
+# number2 = int(input('Num2 : '))
+
+# print(number1 + number2)
+
+
+# اطلب من المستخدم الاسم الاول له 
+# والاسم الاخير 
+# وقم بطباعه  الاسم مقلوب
+
+
+# f_name = input('Enter F Name : ')
+# L_Name = input('Enter L Name : ')
+
+# print(L_Name, f_name)
+
+# name = input('Enter Your Name : ')
+
+# print('Hello', name)
+
+
+# print(5)
+
+# x = 5
+# y = 9
+# z = 10
+# print(x,y,z)
+
+
+# x, y, z = 5, 9, 10
+# print(x,y,z)
+
+# x = [1,69,8,7]
+
+# print(type(5))
+
+
+# print()
+# input()
+# int()
+# str()
+# type()
+# float()
+
+# x = 5
+# print(x)
+
+# x = 1
+
+# print(x)
+# print(float(x))
+
+# x = 5.17 + 10
+# print(type(x))
+
+# print(5**5)
+
+# print(5 % 2)
+
+# 3 + 3 + 3 1
+
+#number = [1,9,87,45,63,2,'abdallah',True,[1,9,8,8]]
+
+#print(number[0] + number[1])
+
+#name = input('Enter Your Name : ')
+
+#print(name)
+
+
+
+# name = ['abdallah']
+
+# name.append('elnakib')
+
+# print(name)
+
+# x = 5
+
+# print(x)
+
+# x = 10
+
+# print(x)
+
+# f_name = input('Enter F Name : ')
+
+# x = 10
+
+# y = 15
+
+# y = 20
