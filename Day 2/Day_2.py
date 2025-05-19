21525a51a6803df2297b4149cb5323edb71eca27
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-05-19 15:07:43 +0300

    add all data from list

--- Day 2/Day_2.py
+++ Day 2/Day_2.py
@@ -1,7 +1,223 @@
+# x = [18,5,2025]
+# print(x[0], '/' , x[1] , '/' , x[2])
+
+# n = input('Number : ')
+# n2 = n + n
+# n3 = n + n + n
+
+# print(int(n) + int(n2) + int(n3))
+
+
+# F_name = input('F Name : ')
+# L_name = input('L Name : ')
+
+# print(L_name, F_name)
+
+# x = []
+
+# x.append(F_name)
+# x.append(L_name)
+
+# x.reverse()
+
+# print(x)
+
+# x = [2,9,87,1]
+# x = ['abdallah', 'ahmed']
+
+# x.sort()
+
+# print(x)
+
+
+# x = [2,9,87,1]
+
+# x.reverse()
+
+# print(x)
+
+# x = [1,6,9,'abdallah',True]
+
+# x.sort()
+
+
+# x = [1,6,9]
+
+# x.append(2)
+
+# x[1] = 'abdallah'
+
+# x.pop(0)
+
+# print(x[0])
+
+
+# x = [1,9,8]
+# x.append('abdallah')
+
+# x.insert(1, 11111)
+# print(x)
+
+# x = ["abdallah", "ahmed","ali", "yasser", "sara", "mohamed"]
+# x[1:5] = ["nader", "tamer",'88888']
+
+
+
+# print(x)
+
+# x = [1,9,8]
+
+
+# x.insert(10, 11111)
+# print(x[3])
+
+
+# x = ["w", "s", "e", "p"]
+
+# x.remove("p")
+
+# print(x)
+
+
+# names = ['abdallah','ahmed','abdallah','ali']
+
+# names.remove('abdallah')
+
+# print(names)
+
+
+# x = [1,5,9,8,5,55,99,5,'ffff',5,9]
+
+# y = int(len(x) / 2)
+# check = len(x) % 2
+
+# if check == 0:
+#     print(x[y], x[y-1])
+# else :
+#     print(x[y])
+
+
+# y = int(len(x) / 2)
+
+# print(x[y])
+
+
+# print(x[-3])
+
+
+# x = [1,5,9,8,5,55,99,5,'ffff',5,9]
+
+
+# print(x[5:-1])
+
+# check = len(x)
+
+# y = x[5:check]
+# y.reverse()
+# print(y)
+
+
+# x = [1,5,9,8,5,55,99,5,'ffff',5,9]
+# o = len(x)
+
+# y = x[5:o]
+
+# print(y)
+
+# x = ["apple", "banana", "cherry"]
+
+# x.clear()
+
+# print(x)
+
+
+# thislist = ["apple", "banana", "cherry"]
+
+# mylist = thislist.copy()
+
+# print(mylist)
+
+
+# x = ["apple", "banana", "cherry"]
+
+# x.append(1)
+# x.append(2)
+
+# x.extend([1,2])
+
+# print(x)
+
+
+# x = [1,9,6,5]
+# y = [8,9,7]
+
+# z = x + y
+
+# print(z)
+
+# x.append()
+# x.pop()
+# x.remove()
+# x.sort()
+# x.insert()
+# x.extend()
+# x.reverse()
+# x.clear()
+# x.copy()
+
+
+# x = [1,9,8,[2,9,7],1,9,[2,9,[2,78,9,5,[8,6,98,7,[2,5,69,8,[23,69]]]]]]
+
+# print(x[6][2][4][4][4][0])
+
+# x = [1,'abdallah',1.5,True,[1,2,[2,2]]]
+
+# print(x[4][2][1])
 
 
 
 
 
 
+
+
+
+
+
+
+# x = [1,9,8,[2,9,7],1,9,[2,9,[2,78,9,5,[8,6,98,7,[2,5,69,8,[23,69]]]]]]
+
+# # print(x[-1][-1][-1][-1][-1][0])
+
+# x1 = x[len(x) -1]
+# x2 = x1[len(x1)-1]
+# x3 = x2[len(x2) -1]
+# x4 = x3[len(x3)-1]
+# x5 = x4[len(x4)-1]
+
+# print(x5[0])
+
+
+
+# x = 'abdallah' # => ['a','b','d','a','a','l','l','a','h']
+
+# y = 'hello my name is abdallah'
+
+# print(y)
+
+# y = 5
+
+# x = ['abdallah',y]
+
+# print(x)
+
+
+
+data = {
+    'name' : 'abdallah',
+    'age' : 27,
+    'phone' : 10265477
+}
+
+print(data['name'])
 
