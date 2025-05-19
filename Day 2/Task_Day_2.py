53a473b47a02f9bd542c368ee79103cd344f6d85
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-05-19 10:27:03 +0300

    add task day 2 and add Source

---
+++ Day 2/Task_Day_2.py
@@ -0,0 +1,21 @@
+# ======================================================================================================
+# Write a Python program to combine two lists into a dictionary. 
+# The elements of the first one serve as keys and the elements of the second one serve as values. 
+# Each item in the first list must be unique and hashable.
+# Sample Output:
+# Original lists:
+# ['a', 'b', 'c', 'd', 'e', 'f']
+# [1, 2, 3, 4, 5]
+# Combine the values of the said two lists into a dictionary:
+# {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+# ======================================================================================================
+# Write a Python program to display the first and last colors from the following list.
+# color_list = ["Red","Green","White" ,"Black"]
+# ======================================================================================================
+# Ask the user for their name, age, city of residence, and job title.
+# After obtaining the data and storing it in a dictionary, ask the user to enter their hobbies from the given options. They will enter numbers for each hobby, for example:
+# 1. Swimming, 2. Climbing, 3. Horseback riding, 4. Cycling.
+# The user enters the numbers 1 or more, and they are added to their personal data. 
+# Then, the text "Hello" is printed, along with the user's name, age, and user's age. 
+# The user's job title is then entered. Then, "Do you have hobbies?" is entered.
+# ======================================================================================================
