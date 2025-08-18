cc72a206b4d746c78d1a09f764c8fc6e7552a4c7
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-08-18 16:48:49 +0300

    add day 5

---
+++ Day-5/index.js
@@ -0,0 +1,46 @@
+// console.log('Test');
+
+// npm start -> npm run start
+// npm install -> npm i
+// npm install express -> npm i express nodemmon
+// npm uninstall express -> npm uninstall express
+// npm update express -> npm update express
+// npm update
+// npm install express@5.0.0 -> npm i express@5.0.0
+
+
+// const x = require('./t1/server');
+
+// console.log(Sum(1,2))
+
+// const { x , y}  = require('./t1/SUm');
+// x(1,2)
+// y(1,2)
+
+// const readline = require('node:readline');
+// const { stdin: input, stdout: output } = require('node:process');
+
+// const rl = readline.createInterface({ input, output });
+
+
+// rl.question('What is your name : ', (answer) => {
+//     console.log(`Hello ${answer}!`);
+//     rl.close();
+// });
+
+// const http = require('http')
+
+// server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.write('Hello World')
+//         res.end()
+
+//     }else if(req.url === '/login') {
+//         res.write('Login Page')
+//         res.end()
+//     }else {
+//         res.write('404')
+//         res.end()
+//     }
+// })
+// server.listen(5000, () => console.log('server is running.......'))
---
+++ Day-5/t1/SUm.js
@@ -0,0 +1,6 @@
+let x = (number1 ,number2) => number1 + number2
+let y = (number1 ,number2) => number1 - number2
+module.exports = {
+    x,
+    y
+}
