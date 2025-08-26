937491537ea8cddf245730c342e447b99faad965
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-08-26 14:08:22 +0300

    check login endpoint

--- Day-11/controllers/loginController.js
+++ Day-11/controllers/loginController.js
@@ -1,8 +1,23 @@
+const {usersData} = require('../models/users');
+const bcrypt = require('bcrypt');
+
 const login = async(req,res) => {
     const {username , password} = req.body;
     if (!username || !password) {
         return res.status(400).json({message : "All inputs are required"})
     }
+    const getUser = await usersData.findOne({username : username}) // {}
+    if (!getUser) {
+        return res.status(400).json({message : "Invalid username"})
+    }
+    const comparePassword = await bcrypt.compare(password, getUser.password)// true or false
+
+    if (!comparePassword) {
+        return res.status(400).json({message : "Invalid password"})
+    }
+
+    return res.json({message : 'login done'})
+
 }
 
 
