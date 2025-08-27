62a207f22a95f0d9c355f5ef2965c526846de743
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-08-27 15:40:04 +0300

    add send otp endpoint

--- Day-12/endPoint.js
+++ Day-12/endPoint.js
@@ -2,8 +2,12 @@ const {app} = require('./index');
 const {login} = require('./controllers/loginController')
 const {register} = require('./controllers/registerController')
 const {logout} = require('./controllers/logoutController')
+const {sendOtp} = require('./controllers/sendOtpController')
+
+
 
 
 app.post('/login', login)
 app.post('/register', register)
 app.post('/logout', logout)
+app.post('/send-otp',sendOtp)
