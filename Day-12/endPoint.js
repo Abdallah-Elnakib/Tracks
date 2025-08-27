c4481dc0c2121ea87736bff41fdfdf89a80e7304
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-08-27 14:54:17 +0300

    add register and logout endpoint

--- Day-12/endPoint.js
+++ Day-12/endPoint.js
@@ -1,5 +1,9 @@
 const {app} = require('./index');
 const {login} = require('./controllers/loginController')
+const {register} = require('./controllers/registerController')
+const {logout} = require('./controllers/logoutController')
 
 
 app.post('/login', login)
+app.post('/register', register)
+app.post('/logout', logout)
