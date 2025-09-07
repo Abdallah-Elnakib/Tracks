597c83ef859d89045bfc53bd1f177e76a136ae4e
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-09-07 17:06:57 +0300

    final day

--- Day-14/index.js
+++ Day-14/index.js
@@ -3,12 +3,55 @@ const authRouter = require('./router/authRouter');
 const productRouter = require('./router/productsRouter');
 const { connectDB } = require('./config/connDB');
 const mongoose = require('mongoose');
+const {checkAuth} = require('./middleware/checkAuth')
 const app = express();
+const { usersData } = require('./models/users')
+
 
 app.use(express.json());
 // 127.0.0.1:3000/
 connectDB()
-app.use('/auth', authRouter)
+
+
+
+
+
+
+
+// app.use('/auth', authRouter)
+
+// app.get('/users', checkAuth, async(req, res) => {
+//     const users = await usersData.find()
+//     res.json(users)
+// })
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
+
+
+// let x = 10
+
+// let x = 20
+
+// console.log(x)
+
+// true = 1
+// false = 0
+
+
+// console.log(typeof(+true))
+// let c = { greeting: "Hey!" };
+// let d;
+
+// d = Object.assign({}, c);
+// d.greeting = "Hello";
+// console.log(c.greeting);
+
 
 mongoose.connection.once('connected', () => {
     console.log('MongoDB connected........');
--- Day-14/middleware/checkAuth.js
+++ Day-14/middleware/checkAuth.js
@@ -3,8 +3,12 @@ const path = require('path');
 const checkAuth = (req, res, next) => {
     const checkuser = fs.readFileSync(path.join(__dirname, '../controllers/token.json'), 'utf8'); // json
     const data = JSON.parse(checkuser); // {"users" : ['abdallah','mahmed'] }
+    const username = req.headers.username;
+    if (!username) {
+        return res.json({ message: "You are not authorized" });
+    }
     for (let user of data.users) {
-        if (user === req.headers.username) {
+        if (user === username) {
             return next();
         }
     }
