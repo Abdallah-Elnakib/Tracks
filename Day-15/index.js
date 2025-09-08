b5bca4f7890b5b076506e5237382430030aee726
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-09-08 14:45:54 +0300

    add endpoint login and logout

--- Day-15/index.js
+++ Day-15/index.js
@@ -3,10 +3,12 @@ const {connectDB} = require('./config/connDB');
 const mongoose = require('mongoose');
 const authRouter = require('./router/authRouter')
 const session = require("express-session");
+const { usersData } = require('./models/users');
+const {checkAuth} = require('./middleware/checkAuth');
+
 const app = express();
 
 app.use(express.json())
-
 
 app.use(session({
   secret: process.env.SESSION_SECRET,   
@@ -22,11 +24,29 @@ app.use(session({
 connectDB()
 
 
-
 app.use('/auth',authRouter) // login register logout
 
 
-mongoose.connection.once('connected' ,()=>{
+
+app.get('/users' ,checkAuth,async(req,res) => {
+    const users = await usersData.find();
+    res.json(users)
+})
+
+app.get('/:username', checkAuth,async(req,res) => {
+    const username  = req.params.username;
+    if (!username) {
+        return res.status(400).send('username  is required');
+    }
+    const getUser = await usersData.findOne({username})
+    if (!getUser) {
+        return res.status(404).send('user not found');
+    }
+    res.json(getUser)
+})
+
+
+mongoose.connection.once('connected' ,()=> {
     console.log("MongoDB connected..............");
     app.listen(process.env.PORT,()=>console.log('Server Runing...........'))
 })
