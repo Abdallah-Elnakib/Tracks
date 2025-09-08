99334e1e3db79059ec6b4a9362a98e3650d40eac
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-09-08 15:39:02 +0300

    add cors middleware

--- Day-15/index.js
+++ Day-15/index.js
@@ -1,44 +1,75 @@
 const express = require('express');
-const {connectDB} = require('./config/connDB');
+const { connectDB } = require('./config/connDB');
 const mongoose = require('mongoose');
 const authRouter = require('./router/authRouter')
 const session = require("express-session");
+const cors = require('cors')
 const { usersData } = require('./models/users');
-const {checkAuth} = require('./middleware/checkAuth');
+const { checkAuth } = require('./middleware/checkAuth');
 
 const app = express();
 
 app.use(express.json())
 
+const ips = ['http://127.0.0.1:3000', "http://localhost:3000"];
+app.use(cors({
+    origin: (origin, callback) => {
+            console.log(origin);
+            if (ips.includes(origin)) {
+                callback(null, true);
+            }
+            else {
+                callback("Not allowed by CORS");
+            }
+
+        
+    }
+}))
+
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// }));
+
+
+
+
 app.use(session({
-  secret: process.env.SESSION_SECRET,   
-  resave: false,           
-  saveUninitialized: false,    
-  cookie: {
-    maxAge: 1000 * 60 ,  
-    httpOnly: true,              
-    secure: false             
-  }
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60,
+        httpOnly: true,
+        secure: false
+    }
 }));
 
 connectDB()
 
 
-app.use('/auth',authRouter) // login register logout
+app.use('/auth', authRouter) // login register logout
 
 
 
-app.get('/users' ,checkAuth,async(req,res) => {
+app.get('/users', async (req, res) => {
     const users = await usersData.find();
     res.json(users)
 })
 
-app.get('/:username', checkAuth,async(req,res) => {
-    const username  = req.params.username;
+app.get('/:username', checkAuth, async (req, res) => {
+    const username = req.params.username;
     if (!username) {
         return res.status(400).send('username  is required');
     }
-    const getUser = await usersData.findOne({username})
+    const getUser = await usersData.findOne({ username })
     if (!getUser) {
         return res.status(404).send('user not found');
     }
@@ -46,12 +77,12 @@ app.get('/:username', checkAuth,async(req,res) => {
 })
 
 
-mongoose.connection.once('connected' ,()=> {
+mongoose.connection.once('connected', () => {
     console.log("MongoDB connected..............");
-    app.listen(process.env.PORT,()=>console.log('Server Runing...........'))
+    app.listen(process.env.PORT, () => console.log('Server Runing...........'))
 })
 
-mongoose.connection.on('error',(err)=>{
+mongoose.connection.on('error', (err) => {
     console.log(err);
 })
 
