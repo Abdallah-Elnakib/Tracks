df3c2f9500bfccc19124653869d3cc51fd0e4c85
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-09-08 12:21:59 +0300

    add register endpoint and handle session sys

--- Day-15/index.js
+++ Day-15/index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const {connectDB} = require('./config/connDB');
 const mongoose = require('mongoose');
 const authRouter = require('./router/authRouter')
-
+const session = require("express-session");
 const app = express();
 
 app.use(express.json())
 
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,   
+  resave: false,           
+  saveUninitialized: false,    
+  cookie: {
+    maxAge: 1000 * 60 ,  
+    httpOnly: true,              
+    secure: false             
+  }
+}));
 
 connectDB()
 
