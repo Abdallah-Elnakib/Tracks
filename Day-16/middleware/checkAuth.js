dd78559848a9b897589ffdda86f46b4cf2e5b445
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-09-09 11:54:19 +0300

    add day 16

--- Day-16/middleware/checkAuth.js
+++ Day-16/middleware/checkAuth.js
@@ -1,10 +1,21 @@
+const jwt = require("jsonwebtoken");
+
 const checkAuth = (req, res, next) => {
-    const getToken = req.session.token;
+    const getToken = req.session.token; 
     if (!getToken) {
         return res.status(401).json({ message: "Unauthorized" });
     } else {
-        return next();
+        jwt.verify(getToken, process.env.JWT_SECRET, (err, decode) => {
+            if (err) {
+                return res.status(401).json({ message: "Unauthorized" });
+            } else {
+                req.user = decode;
+                next();
+            }
+        });
+        
     }
+    // verify(getToken, process.env.JWT_SECRET);
 }
 
 
