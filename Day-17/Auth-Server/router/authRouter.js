f98c462d9c49f21630654dc8b6338000190574b3
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-09-10 12:04:29 +0300

    add auth server and gateway server and add login endpoint in auth server and handle fetch in gateway server

---
+++ Day-17/Auth-Server/router/authRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.post('/login' , (req,res) => {
+    const {username , password} = req.body
+    if (!username || !password) {
+        return res.status(400).json({error : 'Username and password are required'})
+    }
+    res.json({message : 'Login successful'})
+})
+
+
+
+
+
+
+
+module.exports = router
