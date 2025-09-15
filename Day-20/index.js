0cf68d96fd7b173b0192c0332968ff05de2647a6
Abdallah Elnakib <abdallh.elnakeeb@gmail.com>
2025-09-15 14:15:11 +0300

    add soket io

--- Day-20/index.js
+++ Day-20/index.js
@@ -6,6 +6,8 @@ const { Server } = require("socket.io");
 require('dotenv').config({path : path.join(__dirname, ".env")});
 const app = express();
 const httpServer = http.createServer(app);
+const io = new Server(httpServer)
+
 
 app.use(cors())
 app.use(express.json())
@@ -13,6 +15,16 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
 })
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+
+
+
 
 httpServer.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}.........`);
